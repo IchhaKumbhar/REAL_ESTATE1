1d0889b1ed02766d85fac8c58d116c19605e8fbc
IchhaKumbhar <146089024+IchhaKumbhar@users.noreply.github.com>
2024-06-03 17:18:10 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Home from './Components/Home';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { comments } from "@fortawesome/free-solid-svg-icons"
+
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+      
+    </div>
+  );
+}
+
+export default App;
